d418ab5903565c2bbf2d3e2533a4ebdef03644d0
nlaplante <nicolas.laplante@gmail.com>
2013-05-22 22:28:20 -0400

    Generate new site with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularGoogleMapsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
