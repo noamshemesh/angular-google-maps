8dfbdf902cde7bc7d3a7b5574e8fd1d12e673f30
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-07-19 14:22:08 -0400

    Retrieve github stats from the api

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,38 @@
 'use strict';
 
 angular.module('angularGoogleMapsApp')
-  .controller('MainCtrl', function ($scope, $timeout) {
+  .controller('MainCtrl', function ($scope, $log, $timeout, $http) {
     
+	  // list contributors & collaborators
+	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/contributors")
+	  	.success(function (data) {
+	  		$scope.contributors = data;
+	  	});
+	  
+	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/collaborators")
+	  	.success(function (data) {
+	  		$scope.collaborators = data;
+	  		$log.info(data);
+	  	});
     
+	  // latest commit on r1-dev branch
+	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/commits?sha=r1-dev")
+	  	.success(function (data) {
+	  		$scope.latestCommit = _.first(data);
+	  		$log.log($scope.latestCommit);
+	  	});
+	  
+	  $scope.contributed = function () {
+		return _.sortBy(_.uniq(_.union($scope.collaborators, $scope.contributors), false, function (c) {
+			return c && c.login;
+		}), function (c) {
+			return -c.contributions;
+		});  
+	  };
+	  
+	  $scope.isCollaborator = function (login) {
+		return _.where($scope.collaborators, {
+			login: login
+		}).length;
+	  };
   });
