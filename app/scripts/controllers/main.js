06c2cad47e3381e76360de61dd901043477a7617
Nicolas Laplante <nicolas.laplante@gmail.com>
2013-09-06 16:11:35 -0400

    Use github data received in parameter instead of calling the api directly

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,29 +1,13 @@
 'use strict';
 
 angular.module('angularGoogleMapsApp')
-  .controller('MainCtrl', function ($scope, $log, $timeout, $http) {
+  .controller('MainCtrl', function ($scope, $log, $timeout, $http, github) {
     
-	  // list contributors & collaborators
-	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/contributors")
-	  	.success(function (data) {
-	  		$scope.contributors = data;
-	  	});
 	  
-	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/collaborators")
-	  	.success(function (data) {
-	  		$scope.collaborators = data;
-	  		$log.info(data);
-	  	});
-    
-	  // latest commit on r1-dev branch
-	  $http.get("https://api.github.com/repos/nlaplante/angular-google-maps/commits?sha=r1-dev")
-	  	.success(function (data) {
-	  		$scope.latestCommit = _.first(data);
-	  		$log.log($scope.latestCommit);
-	  	});
+	  $scope.github = github;
 	  
 	  $scope.contributed = function () {
-		return _.sortBy(_.uniq(_.union($scope.collaborators, $scope.contributors), false, function (c) {
+		return _.sortBy(_.uniq(_.union($scope.github.collaborators, $scope.github.contributors), false, function (c) {
 			return c && c.login;
 		}), function (c) {
 			return -c.contributions;
@@ -31,7 +15,7 @@ angular.module('angularGoogleMapsApp')
 	  };
 	  
 	  $scope.isCollaborator = function (login) {
-		return _.where($scope.collaborators, {
+		return _.where($scope.github.collaborators, {
 			login: login
 		}).length;
 	  };
